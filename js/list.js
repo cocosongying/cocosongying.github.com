d8ae1e232665b77ab875d3d5db6c8ff710efdae6
宋颖 <cocosongying@gmail.com>
2017-12-31 15:22:52 +0800

    Add files via upload

---
+++ js/list.js
@@ -0,0 +1,9 @@
+var article_list = new Array();
+var blogcnt = 0;
+article_list[blogcnt++] = '<center><h4>磨叽熊猫感谢有您的支持和关注</h4><h3>👇</h3><img src="img/wechatpay.jpeg" width="320" height="320" /></center>\r\n';
+article_list[blogcnt++] = '### 📜 致谢\r\n```\r\n无时无刻都要感谢那些帮助过自己的人，在这里我要感谢父母的养育，老师的教诲，朋友的鼓励，以及来自各方面的帮助，还有正在浏览页面的你，谢谢支持！欢迎分享与收藏 ~~\r\n```\r\n';
+article_list[blogcnt++] = '### 📜 关于\r\n```\r\n初来乍到，多多指教，欢迎通过以下方式联系我：\r\n微博：cocosongying\r\n邮箱：cocosongying@gmail.com```\r\n';
+article_list[blogcnt++] = '### 📜 [游戏](https://cocosongying.github.io/game/)\r\n```\r\n游戏\r\n```\r\n';
+article_list[blogcnt++] = '### 📜 [博客](https://cocosongying.github.io/blog/)\r\n```\r\n技术交流与共享，记录学习中的点点滴滴\r\n```\r\n';
+article_list[blogcnt++] = '### 📜 简介\r\n```\r\ncocosongying\r\n```\r\n';
+// article_list[blogcnt] = '### 📜 [标题](index.html?file=Markdown)\r\n📅 `2018-01-01 00:00` 🔖 `关键词` 🔖 `关键词`\r\n```\r\n摘要\r\n摘要\r\n```\r\n';
